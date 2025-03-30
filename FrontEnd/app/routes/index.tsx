d0c5294d80257c4a9e58018e18ed5f6ad7b8b62c
FrasMaciej <maciejfrs@gmail.com>
2025-03-31 00:47:02 +0200

    Example dashboard test

--- FrontEnd/app/routes/index.tsx
+++ FrontEnd/app/routes/index.tsx
@@ -1,21 +1,11 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { Button } from "@/components/ui/button";
-import { Link } from "@tanstack/react-router";
+import Dashboard from "@/dashboard/dashboard";
 export const Route = createFileRoute("/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <span
-        className="text-4xl font-bold
-      "
-      >
-        Hello
-      </span>
-      <Button>Primary Button</Button>
-      <Link to="/dashboard">Dashboard</Link>
-    </div>
+    <Dashboard/>
   );
 }
