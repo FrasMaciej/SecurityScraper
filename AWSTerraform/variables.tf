0a54e2267d20253878d8bdefe0f6cf73dee69942
FrasMaciej <maciejfrs@gmail.com>
2025-04-03 00:26:23 +0200

    Bucket fix

--- AWSTerraform/variables.tf
+++ AWSTerraform/variables.tf
@@ -2,6 +2,6 @@ variable "region" {
   default = "eu-central-1"
 }
 
-variable "reports_storage_bucket_name" {
-  default = "collector-reports-storage_s3"
+variable "reports_storage_bucket_id" {
+  default = "collector_reports_storage_s3"
 }
