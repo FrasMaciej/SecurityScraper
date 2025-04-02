03aca2295ffec97bd2212ed9d485bf3bcfb034ad
FrasMaciej <maciejfrs@gmail.com>
2025-04-03 00:16:02 +0200

    Fix bucket name param name

--- AWSTerraform/main.tf
+++ AWSTerraform/main.tf
@@ -4,12 +4,12 @@ module "parameter_store" {
 
 module "collector_reports_storage_s3" {
   source      = "./modules/collector_reports_storage_s3"
-  bucket_name = var.bucket_name
+  bucket_name = var.reports_storage_bucket_name
 }
 
 module "shodan_collector_lambda" {
   source      = "./modules/shodan_collector_lambda"
-  bucket_name = var.bucket_name
+  bucket_name = var.reports_storage_bucket_name
 }
 
 module "frontend_lambda" {
