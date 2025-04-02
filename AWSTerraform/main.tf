03149523b67dfa733fdfd6dbd5ca682e2dc7fecd
FrasMaciej <maciejfrs@gmail.com>
2025-04-03 00:43:57 +0200

    naming fix

--- AWSTerraform/main.tf
+++ AWSTerraform/main.tf
@@ -4,15 +4,15 @@ module "parameter_store" {
 
 module "collector_reports_storage_s3" {
   source      = "./modules/collector_reports_storage_s3"
-  bucket_name = var.collector_reports_storage_bucket_name
+  collector_reports_storage_bucket_name = var.collector_reports_storage_bucket_name
 }
 
 module "shodan_collector_lambda" {
   source      = "./modules/shodan_collector_lambda"
-  bucket_name = var.collector_reports_storage_bucket_name
+  collector_reports_storage_bucket_name = var.collector_reports_storage_bucket_name
 }
 
 module "frontend_lambda" {
   source        = "./modules/frontend_lambda"
-  function_name = "security-scraper-frontend"
+  frontend_lambda_name = "security-scraper-frontend"
 }
