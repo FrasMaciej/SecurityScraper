7f965229424928fed438821e49f7da4f6ff75507
FrasMaciej <maciejfrs@gmail.com>
2025-04-03 00:45:55 +0200

    terraform format

--- AWSTerraform/main.tf
+++ AWSTerraform/main.tf
@@ -3,16 +3,16 @@ module "parameter_store" {
 }
 
 module "collector_reports_storage_s3" {
-  source      = "./modules/collector_reports_storage_s3"
+  source                                = "./modules/collector_reports_storage_s3"
   collector_reports_storage_bucket_name = var.collector_reports_storage_bucket_name
 }
 
 module "shodan_collector_lambda" {
-  source      = "./modules/shodan_collector_lambda"
+  source                                = "./modules/shodan_collector_lambda"
   collector_reports_storage_bucket_name = var.collector_reports_storage_bucket_name
 }
 
 module "frontend_lambda" {
-  source        = "./modules/frontend_lambda"
+  source               = "./modules/frontend_lambda"
   frontend_lambda_name = "security-scraper-frontend"
 }
