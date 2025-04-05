2580e4ac3e6950ffbe12d7930a66d7373d2dde60
FrasMaciej <maciejfrs@gmail.com>
2025-04-05 15:02:49 +0200

    Terraform format

--- AWSTerraform/main.tf
+++ AWSTerraform/main.tf
@@ -1,5 +1,5 @@
 module "parameter_store" {
-  source = "./modules/parameter_store"
+  source                      = "./modules/parameter_store"
   shodan_collector_lambda_url = module.shodan_collector_lambda.shodan_collector_lambda_url
 }
 
