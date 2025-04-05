41d707655863ac2c9f18c121ffe0ad6d31c8030b
FrasMaciej <maciejfrs@gmail.com>
2025-04-05 16:29:10 +0200

    Fix env

--- AWSTerraform/modules/frontend_lambda/variables.tf
+++ AWSTerraform/modules/frontend_lambda/variables.tf
@@ -3,12 +3,14 @@ variable "frontend_lambda_name" {
   type        = string
 }
 
-variable "environment_variables" {
-  description = "Environment variables for the Lambda function for fetching data from shodan API"
-  type        = map(string)
-  default     = {}
-} 
-
 variable "shodan_collector_lambda_url" {
   type = string
+}
+
+variable "environment_variables" {
+  description = "Environment variables for the Lambda function for fetching data from Shodan API"
+  type        = map(string)
+  default     = {
+    VITE_SHODAN_LAMBDA_COLLECTOR_URL = var.shodan_collector_lambda_url
+  }
 }
